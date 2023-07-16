271efb5ebc2bc0a7539df56a58c6b30b4efb96e0
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-07-16 19:16:33 +0600

    added configuration and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'postgres'>('TYPEORM_CONNECTION'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
